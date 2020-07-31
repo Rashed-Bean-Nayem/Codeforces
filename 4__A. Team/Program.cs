2bdebc10040e439d4dc857745ab26e4eb1746892
Rashed <rashedamlan02@gmail.com>
2020-07-31 08:29:50 +0600

    CodeForces

---
+++ 4__A. Team/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace _4__A._Team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+
+            int[,] arr = new int[size, 3];
+
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+            int count = 0;
+            int result = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+
+                    count = count + arr[i, j];
+
+                }
+                if (count >= 2)
+                {
+                    result = result + 1;
+
+                    count = 0;
+                }
+
+            }
+
+
+
+            Console.WriteLine(result);
+            //int n = 0, i, sum = 0, count = 0;
+            //// scanf("%d", &n);
+            //n = Convert.ToInt32(Console.ReadLine());
+            //int[] a = new int[5];
+
+            //do
+            //{
+            //    for (i = 0; i < 3; i++)
+            //    {
+            //        // scanf("%d", &a[i]);
+            //        a[i] = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //    sum = 0;
+            //    for (i = 0; i < 3; i++)
+            //    {
+            //        if (a[i] == 1) sum++;
+            //    }
+            //    if (sum >= 2) count++;
+
+            //    n--;
+            //} while (n > 0);
+            ////printf("%d\n", count);
+
+            //Console.WriteLine(count);
+            ////  return 0;
+
+
+
+
+
+        }
+    }
+}
---
+++ 6__A. String Task/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _6__A._String_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+
+        Come:
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
+                {
+                    string jk = input.Remove(input.IndexOf(input[i]), 1);
+                    input = jk;
+                    goto Come;
+                }
+
+
+
+            }
+
+
+            //string   str1 = "ppp";
+            
+
+            for (int i = 0; i < input.Length; i=i+2)
+            {
+                string strRes = input.Insert(i, ".");
+                Console.WriteLine(strRes.ToString());
+            }
+
+            
+
+
+
+
+
+            //Console.WriteLine(input);
+        }
+    }
+}

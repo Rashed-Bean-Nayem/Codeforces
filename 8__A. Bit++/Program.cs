ee70c667234ee6be6c28aed3375685216d189d5d
Rashed <rashedamlan02@gmail.com>
2020-07-31 20:55:24 +0600

    CodeForces

---
+++ 8__A. Bit++/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _8__A._Bit__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int p = 0;
+
+            for (int i = 0; i < n; i++)
+
+            {
+                string b = Console.ReadLine();
+
+                if (b[0] == '-' || b[2] == '-')
+
+                {
+                    p = p - 1;
+                }
+
+                else if (b[0] == '+' || b[2] == '+')
+
+                {
+                    p = p + 1;
+                }
+            }
+            Console.WriteLine(p);
+
+
+
+
+        }
+    }
+}

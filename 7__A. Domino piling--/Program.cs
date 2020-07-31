fda8b9c8758d2da6dd9938d41bd0cc0faebda32d
Rashed <rashedamlan02@gmail.com>
2020-07-31 18:37:46 +0600

    CodeForces

---
+++ 7__A. Domino piling--/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _7__A._Domino_piling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+
+
+            n = Convert.ToInt32(Console.ReadLine());
+            k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine((n * k) / 2);
+        }
+    }
+}

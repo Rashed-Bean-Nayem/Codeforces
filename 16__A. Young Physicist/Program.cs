d689225d9e1aaf974c9fe01a1ada0ec1e259aa36
Rashed <rashedamlan02@gmail.com>
2020-08-09 14:22:41 +0600

    CodeForces

---
+++ 16__A. Young Physicist/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _16__A._Young_Physicist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int size = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                int[,] input = new int[size,3];
+
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    input[i,j]= int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+
+
+
+            int result = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    result += input[i,j];
+                }
+            }
+
+
+
+            if (result==0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+
+        }
+    }
+}

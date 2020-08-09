0d87e6bc3dc4f3d20501845274b5b858610b3b62
Rashed <rashedamlan02@gmail.com>
2020-08-09 23:31:42 +0600

    CodeForces

--- 16__A. Young Physicist/Program.cs
+++ 16__A. Young Physicist/Program.cs
@@ -6,46 +6,64 @@ namespace _16__A._Young_Physicist
     {
         static void Main(string[] args)
         {
-            
-                int size = int.Parse(Console.ReadLine());
-                Console.WriteLine();
 
-                int[,] input = new int[size,3];
+            //   int size = int.Parse(Console.ReadLine());
+
+            //int sum1 = 0,a;
+            //int sum2 = 0,b;
+            //int sum3 = 0,c;
 
 
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < 3; j++)
+            //for (int i = 0; i < size; i++)
+            //{
+            //     a = int.Parse(Console.ReadLine());
+            //     b = int.Parse(Console.ReadLine());
+            //     c = int.Parse(Console.ReadLine());
+
+            //    sum1 += a;
+            //    sum2 += b;
+            //    sum3 += c;
+
+            //}
+            //if (sum1 == 0 && sum2 == 0 && sum3 == 0)
+            //    Console.WriteLine("YES");
+            //else
+            //    Console.WriteLine("NO");
+
+
+           
+
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int xsum = 0, ysum = 0, zsum = 0;
+
+                for (int j = 0; j < n; j++)
+
                 {
-                    input[i,j]= int.Parse(Console.ReadLine());
+                    string s = Console.ReadLine();
+                    string[] values = s.Split(' ');
+                    int x = int.Parse(values[0]);
+                    int y = int.Parse(values[1]);
+                    int z = int.Parse(values[2]);
+                    xsum += x;
+                    ysum += y;
+                    zsum += z;
                 }
-                Console.WriteLine();
-            }
 
+                if (xsum == 0 && ysum == 0 && zsum == 0)
 
-
-            int result = 0;
-
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < 3; j++)
                 {
-                    result += input[i,j];
+                    Console.WriteLine("YES");
                 }
+
+                else
+
+                {
+                    Console.WriteLine("NO");
+                }
+
             }
-
-
-
-            if (result==0)
-            {
-                Console.WriteLine("YES");
-            }
-            else
-            {
-                Console.WriteLine("NO");
-            }
-
 
         }
     }
-}
+

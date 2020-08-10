1a02198bd6aa8d09b1639d673e90f2972759c5af
Rashed <rashedamlan02@gmail.com>
2020-08-10 12:21:00 +0600

    CodeForces

--- 10__A. Beautiful Matrix/Program.cs
+++ 10__A. Beautiful Matrix/Program.cs
@@ -21,122 +21,24 @@ namespace _10__A._Beautiful_Matrix
                 {
                    
                 arr[i, j] = int.Parse(values[j]);
-                }
-                Console.WriteLine();
-            }
 
 
-          
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
                     if (arr[i, j] == 1)
                     {
                         i_ = i;
                         j_ = j;
                     }
-                }
-
-            }
-           // Console.WriteLine(i_ + " " + j_);
-
-
-
-            if ((i_ < 2 && j_ <= 4)   || (i_ == 2 && j_ < 2 ))
-            {
-
-                arr[2, 2] = 1;
-
-                for (int i = i_; i < 5; i++)
-                {
-                    for (int j = j_ + 1; j < 5; j++)
-                    {
-                        if (arr[i, j] != 1)
-                        {
-                            count++;
-                        }
-                        else if (arr[i, j] == 1)
-                        {
-                            count++;
-                            goto rtrt;
-
-                        }
-                    }
-                    break;
-                }
-
-
-                for (int i = i_ + 1; i < 5; i++)
-                {
-                    for (int j = 0; j < 5; j++)
-                    {
-                        if (arr[i, j] != 1)
-                        {
-                            count++;
-                        }
-                        else if (arr[i, j] == 1)
-                        {
-                            count++;
-                            goto rtrt;
-
-                        }
-                    }
 
                 }
-
-
-            rtrt:
-                Console.WriteLine(count);
+                Console.WriteLine();
             }
 
-            else if(j_ !=2)
-            {
-                for (int i = 2; i < 5; i++)
-                {
-                    for (int j = 3; j < 5; j++)
-                    {
-                        if (arr[i, j] != 1)
-                        {
-                            count++;
-                        }
-                        else if (arr[i, j] == 1)
-                        {
-                            count++;
-                            goto rtrt2;
 
-                        }
-                    }
-                    break;
-                }
+            int ans1 = Math.Abs(2-i_);
+            int ans2 = Math.Abs(2-j_);
 
-                for (int i = 3; i < 5; i++)
-                {
-                    for (int j = 0; j < 5; j++)
-                    {
-                        if (arr[i, j] != 1)
-                        {
-                            count++;
-                        }
-                        else if (arr[i, j] == 1)
-                        {
-                            count++;
-                            goto rtrt2;
 
-                        }
-                    }
-
-                }
-
-            rtrt2:
-                Console.WriteLine(count);
-            }
-            else
-            {
-          
-                Console.WriteLine(count);
-            }
-
+            Console.WriteLine(ans1+ans2);
         
 
         }

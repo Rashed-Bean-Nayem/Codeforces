6e9e2fd138616b337413ffa02bdb729eef520e1b
Rashed <rashedamlan02@gmail.com>
2020-08-11 11:18:28 +0600

    CodeForces

--- 19__A. Lucky Division/Program.cs
+++ 19__A. Lucky Division/Program.cs
@@ -6,9 +6,9 @@ namespace _19__A._Lucky_Division
     {
         static void Main(string[] args)
         {
-            int rt = int.Parse(Console.ReadLine());
-            string hj = rt.ToString();
-            //  Console.WriteLine(hj);
+            int n = int.Parse(Console.ReadLine());
+            string hj = n.ToString();
+           
 
             int result = 0;
 
@@ -21,29 +21,25 @@ namespace _19__A._Lucky_Division
                 {
                     result++;
                 }
-
-                //Console.WriteLine(hj[i]);
-
             }
 
-         //   Console.WriteLine(hj[0]);
-
            
-
             if (result == hj.Length)
             {
                 Console.WriteLine("YES");
             }
-            else if (rt % 4 == 0 || rt % 7 == 0)
+            else if (((n % 4) == 0) || ((n % 7) == 0) || ((n % 44) == 0) || ((n % 47) == 0) 
+                || ((n % 74) == 0) || ((n %444) == 0) || ((n % 447) == 0) || ((n % 474) == 0)
+                || ((n % 477) == 0) || ((n % 777) == 0) || ((n % 774) == 0) || ((n % 744) == 0) 
+                || ((n % 77) == 0) || ((n % 747) == 0))
             {
                 Console.WriteLine("YES");
             }
             else
             {
-                Console.WriteLine("NO");   
+                Console.WriteLine("NO");
             }
-
-
+            // 4,7,44,47,74,,77,444,447,474,477,747,777,774,744
         }
     }
 }
